b7882669cc097b94d2e85b322fa2d38a4716c7fc
Saif-Rahman-801 <saifurs102@gmail.com>
2024-01-29 20:06:43 +0600

    using middlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import connectDB from "./db/index.js";
-import 'dotenv/config'
+import "dotenv/config";
+import { app } from "./app.js";
 
 connectDB()
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("app listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed (from src/index.js)", error);
+  });
 
 /* const app = express()
 
