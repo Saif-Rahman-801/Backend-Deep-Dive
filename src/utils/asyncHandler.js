d77bf63aa8973bae9b546b4b825e52ccd1ddefcb
Saif-Rahman-801 <saifurs102@gmail.com>
2024-01-30 12:23:45 +0600

    utils written

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+// writing utility function for async operations
+
+/* // 1. with async await
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+    try {
+        await requestHandler(req, res, next)
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        mssg: error.message,
+      });
+    }
+}; */
+
+/* const asyncHandlerSyntaxExplained = (requestHandler) => {
+    async (req, res, next) => {
+      try {
+          await requestHandler(req, res, next)
+      } catch (error) {
+        res.status(error.code || 500).json({
+          success: false,
+          mssg: error.message,
+        });
+      }
+    };
+  }; */
+
+// 2. with promise
+const asyncHandler = (requestHandler) => {
+  async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
